3e15d87f892ad3d62cea0463c55f5cd17ff6116a
Griffin <griffin.z.s@outlook.com>
2023-02-16 21:16:08 -0800

    update to linux

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,7 @@
+local wezterm = require 'wezterm'
+
+return {
+  font = wezterm.font 'Fira Code',
+  font_size = 10,
+  color_scheme = 'nord',
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# dotfiles ⚫️🔴🔵⚪️
+
+A collection of dotfiles used in my Unix-like environments. Typically MacOS and a flavor of Arch Linux.
